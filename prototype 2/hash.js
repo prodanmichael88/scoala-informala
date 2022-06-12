6f750731af58756b8741b71f4d58f8a87d2341a1
Marius Prodan <107351846+prodanmichael88@users.noreply.github.com>
2022-06-12 16:23:10 +0300

    Delete hash.js

--- prototype 2/hash.js
+++
@@ -1,9 +0,0 @@
-
-let myNav = document.querySelector('nav');
-myNav.onclick = function() {
-    window.location.hash = '#food';
-    myNav.style.backgroundColor='blue';
-   // console.log(url.hash);
-    
-}
-console.log(url.href);
